f7935da176fd23799934830ebc443f67bfdb4948
Russell Tolentino <rjtolent@hawaii.edu>
2019-04-21 16:31:47 -1000

    Box Farm GUI v0.5.3
    Pump and light time input.
    
    Changes:
    + Fixed layout.
    + Changed dropdowns to light gray (btn-light).
    + Indentation is made to be two spaces.
    
    Added:
    
    Removed:
    
    Issues:
    + Pump times need to be a list.

--- gui/public/data_types.js
+++ gui/public/data_types.js
@@ -15,63 +15,62 @@ function Time( timeStr ) {
   
   // Assume it is in the order of hours, minutes, and seconds.
   for( var i = 0; i < timeArr.length; i++ ) {
-	// Convert each hour, minute, and second from string to integer.
+    // Convert each hour, minute, and second from string to integer.
     hhmmss.push( timeArr[ i ]|0 )
   }
   
   // Check if there are enough digits that represent time.
   if( hhmmss.length === 3 ) {
-	// No need to do anything.
+    // No need to do anything.
   } else if( hhmmss.length === 2 ) {
-	// Set the seconds (should be the last entry) to zero.
-	hhmmss.push( 0 );
+    // Set the seconds (should be the last entry) to zero.
+    hhmmss.push( 0 );
   } else {
-	console.error( "Time Format Error: Time string is not parsed correctly." );
-	
-	// End here.
-	return;
+    console.error( "Time Format Error: Time string is not parsed correctly." );
+    
+    // End here.
+    return;
   }
   
   // Check if hours, minutes, and seconds are valid values.
   /* Add here. */
   
   this.getTime = function() {
-	return {
-      hr: hhmmss[ 0 ],
-	  min: hhmmss[ 1 ],
-	  sec: hhmmss[ 2 ]
-	};
+    return {
+       hr: hhmmss[ 0 ],
+      min: hhmmss[ 1 ],
+      sec: hhmmss[ 2 ]
+    };
   };
   
   // In { hr: ..., min: ..., sec: ... }
   this.setTime = function( hhmmssObj ) {
-	hhmmss[ 0 ] = hhmmssObj.hr;
-	hhmmss[ 1 ] = hhmmssObj.min;
-	
-	// Set the seconds to zero if unspecified.
-	if( typeof hhmmssObj.sec === "number" ) {
-	  hhmmss[ 2 ] = hhmmssObj.sec;
-	} else {
-      hhmmss[ 2 ] = 0;
-	}
+    hhmmss[ 0 ] = hhmmssObj.hr;
+    hhmmss[ 1 ] = hhmmssObj.min;
+    
+    // Set the seconds to zero if unspecified.
+    if( typeof hhmmssObj.sec === "number" ) {
+      hhmmss[ 2 ] = hhmmssObj.sec;
+    } else {
+          hhmmss[ 2 ] = 0;
+    }
   };
   
   this.setHr = function( hr ) {
-	hhmmss[ 0 ] = hr;
+    hhmmss[ 0 ] = hr;
   };
   
   this.setMin = function( min ) {
-	hhmmss[ 1 ] = min;
+    hhmmss[ 1 ] = min;
   };
   
   this.setSec = function( sec ) {
-	hhmmss[ 2 ] = sec;
+    hhmmss[ 2 ] = sec;
   };
   
   // Utility functions.
-  
-  // Add zero in front of digit. Always returns a string.
+  // Add zero in front of time digits. Always returns a string.
   this.doubleDigit = function( timeVal ) {
-	return timeVal < 10 ? "0" + timeVal : timeVal;
+    return timeVal < 10 ? "0" + timeVal : timeVal;
   }
 }
--- gui/public/ui_logic.js
+++ gui/public/ui_logic.js
@@ -1,4 +1,10 @@
 /**
+ * 
+ * Place for interface controls and logic.
+ * 
+ */
+
+/**
  * @constructor
  * Generate graphics for the time selector.
  * Make sure its parent element is a block element.
@@ -12,8 +18,11 @@
 function InputTimeDropdown( container, type, timeObj ) {
   // Constants.
   var nHh = 24;
+  var stepHh = 1;
   var nMm = 60;
+  var stepMm = 5;
   var nSs = 60;
+  var stepSs = 5;
   
   // Check if the container is not for the dropdown. Make sure this is specified first.
   if( container.classList[ 0 ] !== "dropdown" ) {
@@ -23,7 +32,9 @@ function InputTimeDropdown( container, type, timeObj ) {
   // Dropdown face in the container.
   var face = document.createElement( "button" );
   face.type = "button";
-  face.classList.add( "btn", "btn-primary", "dropdown-toggle" );
+  
+  // Dropdown color here. ----V
+  face.classList.add( "btn", "btn-light", "dropdown-toggle" );
   face.setAttribute( "data-toggle", "dropdown" );
   
   // Dropdown menu in the container.
@@ -38,103 +49,106 @@ function InputTimeDropdown( container, type, timeObj ) {
   var selectAction = function( event ) {};
   
   switch( type ) {
-	case "hr":
-	  for( var i = 0; i < nHh; i++ ) {
+    case "hr":
+      for( var i = 0; i < nHh/stepHh; i++ ) {
         entryList[ i ] = document.createElement( "div" );
-		entryList[ i ].classList.add( "dropdown-item" );
-		entryList[ i ].setAttribute( "data-number", i );
-		entryList[ i ].textContent = i;
-	  }
-	  
-	  // Show the hour value.
-	  face.textContent = timeObj.doubleDigit( timeObj.getTime().hr );
-	  
-	  selectAction = function() {
-		var selectedValue = event.target.getAttribute( "data-number" );
-	  
-		// Convert from string to integer.
-		var selectedValueNumber = selectedValue|0;
-		  
-		// Only do something when it selects an entry from the menu.
-		if( typeof selectedValue !== "object" ) {
-		  // Modify the timeObj.
-		  timeObj.setHr( selectedValueNumber );
-		  
-		  // Show the hour value.
-		  face.textContent = timeObj.doubleDigit( timeObj.getTime().hr );
-		}
-	  }
-	  
-	  break;
-	case "min":
-	  for( var i = 0; i < nMm; i++ ) {
+        entryList[ i ].classList.add( "dropdown-item" );
+        entryList[ i ].setAttribute( "data-number", stepHh*i );
+        entryList[ i ].textContent = timeObj.doubleDigit( stepHh*i );
+      }
+      
+      // Show the hour value.
+      face.textContent = timeObj.doubleDigit( timeObj.getTime().hr );
+      
+      selectAction = function( event ) {
+        var selectedValue = event.target.getAttribute( "data-number" );
+        
+        // Convert from string to integer.
+        var selectedValueNumber = selectedValue|0;
+          
+        // Only do something when it selects an entry from the menu.
+        if( typeof selectedValue !== "object" ) {
+          // Modify the timeObj.
+          timeObj.setHr( selectedValueNumber );
+          
+          // Show the hour value.
+          face.textContent = timeObj.doubleDigit( timeObj.getTime().hr );
+        }
+      }
+      
+      break;
+    case "min":
+      for( var i = 0; i < nMm/stepMm; i++ ) {
         entryList[ i ] = document.createElement( "div" );
-		entryList[ i ].classList.add( "dropdown-item" );
-		entryList[ i ].setAttribute( "data-number", i );
-		entryList[ i ].textContent = i;
-	  }
-	  
-	  // Show the minute value.
-	  face.textContent = timeObj.doubleDigit( timeObj.getTime().min );
-	  
-	  selectAction = function() {
-		var selectedValue = event.target.getAttribute( "data-number" );
-	  
-		// Convert from string to integer.
-		var selectedValueNumber = selectedValue|0;
-		  
-		// Only do something when it selects an entry from the menu.
-		if( typeof selectedValue !== "object" ) {
-		  // Modify the timeObj.
-		  timeObj.setMin( selectedValueNumber );
-		  
-		  // Show the minute value.
-		  face.textContent = timeObj.doubleDigit( timeObj.getTime().min );
-		}
-	  }
-	  
-	  break;
-	case "sec":
-	  for( var i = 0; i < nSs; i++ ) {
+        entryList[ i ].classList.add( "dropdown-item" );
+        entryList[ i ].setAttribute( "data-number", stepMm*i );
+        entryList[ i ].textContent = timeObj.doubleDigit( stepMm*i );
+      }
+      
+      // Show the minute value.
+      face.textContent = timeObj.doubleDigit( timeObj.getTime().min );
+      
+      selectAction = function( event ) {
+        var selectedValue = event.target.getAttribute( "data-number" );
+        
+        // Convert from string to integer.
+        var selectedValueNumber = selectedValue|0;
+          
+        // Only do something when it selects an entry from the menu and
+        // not anywhere else on the menu.
+        if( typeof selectedValue !== "object" ) {
+          // Modify the timeObj.
+          timeObj.setMin( selectedValueNumber );
+          
+          // Show the minute value.
+          face.textContent = timeObj.doubleDigit( timeObj.getTime().min );
+        }
+      }
+      
+      break;
+    case "sec":
+      for( var i = 0; i < nSs/stepSs; i++ ) {
         entryList[ i ] = document.createElement( "div" );
-		entryList[ i ].classList.add( "dropdown-item" );
-		entryList[ i ].setAttribute( "data-number", i );
-		entryList[ i ].textContent = i;
-	  }
-	  
-	  // Show the second value.
-	  face.textContent = timeObj.doubleDigit( timeObj.getTime().sec );
-	  
-	  selectAction = function() {
-		var selectedValue = event.target.getAttribute( "data-number" );
-	  
-		// Convert from string to integer.
-		var selectedValueNumber = selectedValue|0;
-		  
-		// Only do something when it selects an entry from the menu.
-		if( typeof selectedValue !== "object" ) {
-		  // Modify the timeObj.
-		  timeObj.setSec( selectedValueNumber );
-		  
-		  // Show the hour value.
-		  face.textContent = timeObj.doubleDigit( timeObj.getTime().sec );
-		}
-	  }
-	  
-	  break;
+        entryList[ i ].classList.add( "dropdown-item" );
+        entryList[ i ].setAttribute( "data-number", stepSs*i );
+        entryList[ i ].textContent = timeObj.doubleDigit( stepSs*i );
+      }
+      
+      // Show the second value.
+      face.textContent = timeObj.doubleDigit( timeObj.getTime().sec );
+      
+      selectAction = function( event ) {
+      var selectedValue = event.target.getAttribute( "data-number" );
+      
+      // Convert from string to integer.
+      var selectedValueNumber = selectedValue|0;
+        
+      // Only do something when it selects an entry from the menu.
+      if( typeof selectedValue !== "object" ) {
+          // Modify the timeObj.
+          timeObj.setSec( selectedValueNumber );
+          
+          // Show the hour value.
+          face.textContent = timeObj.doubleDigit( timeObj.getTime().sec );
+        }
+      }
+      
+      break;
     default:
-	  console.error( "Time error: Type is not either in hours, minutes, or seconds." );
+      console.error( "Time error: Type is not either in hours, minutes, or seconds." );
   }
 
   // Set the time based on which menu entry is clicked.
   menu.addEventListener( "mouseup", selectAction );
   
-  // Append the time entries in the menu.
-  for( var i = 0; i < entryList.length; i++ ) {
-	menu.appendChild( entryList[ i ] );
-  }
-  
-  // Append these elements to the container element.
-  container.appendChild( face );
-  container.appendChild( menu );
+  this.applyAndShow = function() {
+    // Append the time entries in the menu.
+    for( var i = 0; i < entryList.length; i++ ) {
+      menu.appendChild( entryList[ i ] );
+    }
+      
+    // Append these elements to the container element.
+    container.appendChild( face );
+    container.appendChild( menu );
+  };
 }
